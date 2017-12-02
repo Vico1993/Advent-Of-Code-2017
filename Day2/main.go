373e04929fbf367d301ef3b90b2d323f4f45fd27
Victor Piolin <victor.piolin@gmail.com>
2017-12-02 13:50:47 +0100

    Solution Day 2 Part 2

--- Day2/main.go
+++ Day2/main.go
@@ -1,5 +1,57 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
+func splitStringInToInt(s string) []int {
+	var tmp = strings.Split(s, "\t")
+	var digits = make([]int, len(tmp))
+
+	for i := range tmp {
+		var val, err = strconv.Atoi(tmp[i])
+		if err != nil {
+			log.Fatal("Error convert String to Int", err)
+		}
+		digits[i] = val
+	}
+
+	return digits
+}
+
+func main() {
+	// Open File
+	file, _ := os.Open("input.txt")
+	fscanner := bufio.NewScanner(file)
+
+	var sum = 0
+	for fscanner.Scan() {
+		var min = 999999999
+		var max = 0
+
+		var line = splitStringInToInt(fscanner.Text())
+		fmt.Println("for the line: ", line)
+		for _, v := range line {
+			if v <= min {
+				min = v
+			}
+
+			if v >= max {
+				max = v
+			}
+		}
+
+		fmt.Println("Max:", max)
+		fmt.Println("Min:", min)
+		fmt.Println("Sum Before:", sum)
+		sum += max - min
+		fmt.Println("Sum After:", sum)
+	}
+
+	fmt.Println("sum:", sum)
 }
