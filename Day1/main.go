e8be64eb4bf613cb6ed1ae2937c310135d95d1ad
Victor Piolin <victor.piolin@gmail.com>
2017-12-02 10:00:59 +0100

    ParseInt to array

--- Day1/main.go
+++ Day1/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"io/ioutil"
+	"log"
+	"strconv"
+)
+
+func parseToInt(s string) []int {
+	digits := make([]int, len(s))
+	for i := range s {
+		val, err := strconv.Atoi(s[i : i+1])
+		if err != nil {
+			log.Fatal("Error")
+		}
+		println(val)
+	}
+	return digits
+}
+
 func main() {
+	data, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal("Error on the file")
+	}
+
+	// Convert Bytes to String
+	dataStr := string(data[:])
+	parseToInt(dataStr)
 
 }
