86f279d8bcc52141f9bb46593ed6797ffbf37afe
Victor Piolin <victor.piolin@gmail.com>
2017-12-02 14:29:34 +0100

    Day2 PArt 2 Dividor

--- Day2/main.go
+++ Day2/main.go
@@ -24,29 +24,51 @@ func splitStringInToInt(s string) []int {
 	return digits
 }
 
+func findMaxAndMin(l []int) (int, int) {
+	var min = 999999999
+	var max = 0
+
+	for _, v := range l {
+		if v <= min {
+			min = v
+		}
+
+		if v >= max {
+			max = v
+		}
+	}
+
+	return max, min
+}
+
+func findDividor(l []int) int {
+	var mod = 0
+	for _, v := range l {
+		for _, v2 := range l {
+			if v != v2 && v%v2 == 0 {
+				mod = v / v2
+			}
+		}
+	}
+
+	return mod
+}
+
 func main() {
 	// Open File
 	file, _ := os.Open("input.txt")
 	fscanner := bufio.NewScanner(file)
 
 	var CheckSumPart1 = 0
+	var CheckSumPart2 = 0
 	for fscanner.Scan() {
-		var min = 999999999
-		var max = 0
 
 		var line = splitStringInToInt(fscanner.Text())
-		for _, v := range line {
-			if v <= min {
-				min = v
-			}
-
-			if v >= max {
-				max = v
-			}
-		}
-
+		var max, min = findMaxAndMin(line)
 		CheckSumPart1 += max - min
+		CheckSumPart2 += findDividor(line)
 	}
 
 	fmt.Println("CheckSumPart1 PART 1:", CheckSumPart1)
+	fmt.Println("CheckSumPart2 PART 2:", CheckSumPart2)
 }
